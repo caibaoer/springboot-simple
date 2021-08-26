91802cc5eb49d31fe21809ee7a9baf5723df9e2c
huangwc <huangwc@songcw.com>
2021-08-26 16:58:02 +0800

    1

---
+++ src/main/java/com/controller/TestController.java
@@ -0,0 +1,18 @@
+package com.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Classname TestController
+ * @Description TODO
+ * @Date 2021/8/25 17:14
+ * @Created by huangwencai
+ */
+@RestController
+public class TestController {
+    @RequestMapping("test")
+    public String test(){
+        return "test";
+    }
+}
